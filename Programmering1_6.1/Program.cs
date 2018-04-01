aef006df747b10b803b31cf0ef3964007d6a6884
Monica Björk <m.bjork95@gmail.com>
2018-04-01 21:19:31 +0200

    Add project files.

---
+++ Programmering1_6.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_6._1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
